9e7ee8439cb8fbf9af82d666e8ee170b18025d85
razawat <70076045+razawat@users.noreply.github.com>
2024-01-21 02:02:43 +0530

    Navbar dev - navbar skelaton done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavBarComponent } from './Component/nav-bar/nav-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:NavBarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
